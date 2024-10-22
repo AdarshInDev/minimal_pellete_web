0a43a5208b0858a2ebec43070b6d5262b90304ea
Adarsh Pradhan <dev.apradhan1508@gmail.com>
2024-10-22 23:51:20 +0530

    init

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"edd8546116457bdf1c5bdfb13ecb9463d2bb5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2429636926"
+    serviceWorkerVersion: "2885625528"
   }
 });
